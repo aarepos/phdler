5b753dfa725d12c963e657ef841ad6bcc1cf6a91
aarepos <ca5bd9@gmail.com>
2021-09-24 00:43:10 +0330

    YT Download

--- plugins/start.py
+++ plugins/start.py
@@ -7,12 +7,5 @@ def start(client, message):
     chat_id = message.chat.id
     client.send_message(
         chat_id,
-        "please choose",
-        reply_markup=ReplyKeyboardMarkup(
-            [
-                ["Download from YT", "Download from PH"],  # First row
-                ["Communication with Admin(A-Group)"]  # second row
-            ],
-            resize_keyboard=True
-        )
+        "لطفا دستور/DYTسپس لینک را وارد کنید مانند زیر \n /DYT https:\\....."
     )
--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,1 +1,23 @@
+from __future__ import unicode_literals
 from pyrogram import Client, filters
+import youtube_dl
+
+
+def downloadYT(url):
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+@app.on_message(filters=filters.command("DYT"))
+def downloadAndSendVideo(client, message):
+    chat_id = message.chat.id
+    url = message.command[1]
+    video = downloadYT(url)
+    client.send_video(
+        chat_id=chat_id,
+        video="Havanna Winter-9wZqI0JKKhk.mp4"
+    )
+
+
+
